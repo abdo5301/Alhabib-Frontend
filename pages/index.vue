b3336cc7a5c21e5bfb4ecac5bcf9c502846a35e8
Abdulrahman Saeed <abdelrahmansaeed3@gmail.com>
2023-07-27 10:47:02 +0300

    Index Category Section

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,40 @@
 <template>
  <HomeFirstBanner />
  <HomeSecondBanner />
+ <HomeCategorySection :categories="bed_categories" class="mb-[93px]">تسوق مستلزمات السرير</HomeCategorySection>
+
 </template>
 
 <script setup>
+
+const bed_categories = [
+  {
+    id: 1,
+    name: 'لباد وواقي مرتبة',
+    image:'/images/category/lbad.png',
+    href:'#'
+  },
+  {
+    id: 2,
+    name: 'مفارش وبطانيات',
+    image:'/images/category/bedding.png',
+    href:'#'
+
+  },
+  {
+    id: 3,
+    name: 'وسائد وخداديات',
+    image:'/images/category/cushions.png',
+    href:'#'
+  },
+  {
+    id: 4,
+    name: 'الأطفال والمواليد',
+    image:'/images/category/baby.png',
+    href:'#'
+  },
+
+]
+
 
 </script>
