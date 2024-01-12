7ea3627ca15842d24f81e8cdf29c3603d845c8a8
alhabib shop <info@alhabibshop.com>
2024-01-12 23:51:14 +0300

    add log

--- components/checkout/Payment.vue
+++ components/checkout/Payment.vue
@@ -116,65 +116,109 @@ const { cartTotal } = useCart()
 const config = useRuntimeConfig()
 const selectedApplePayMethod = ref(false)
 
-const onApplePayButtonClicked = async () => {
+function onApplePayButtonClicked() {
 
-    if (!ApplePaySession) {
-      console.log('123');
-      return;
-    }
-
-    // Define ApplePayPaymentRequest
-    const request = {
-      countryCode: 'SA',
-      currencyCode: 'SAR',
-      supportedNetworks: ["visa", "masterCard", "amex", "discover", "mada"],
-      merchantCapabilities: ["supports3DS"],
-      total: { label: "alhabibshop", amount: cartTotal.value }
-    };
-
-    // Create ApplePaySession
-    const session = new ApplePaySession(6, request);
-
-    console.table(session)
-    console.log('140')
-
-    session.onvalidatemerchant = async event => {
-      console.table(event)
-      // Call your own server to request a new merchant session.
-      const merchantSession = await validateMerchant();
-      session.completeMerchantValidation(merchantSession);
-    };
-
-    session.onpaymentmethodselected = event => {
-      // Define ApplePayPaymentMethodUpdate based on the selected payment method.
-      // No updates or errors are needed, pass an empty object.
-      const update = {};
-      session.completePaymentMethodSelection(update);
-    };
-
-    session.onshippingmethodselected = event => {
-      // Define ApplePayShippingMethodUpdate based on the selected shipping method.
-      // No updates or errors are needed, pass an empty object.
-      const update = {};
-      session.completeShippingMethodSelection(update);
-    };
-
-
-    session.onpaymentauthorized = event => {
-      // Define ApplePayPaymentAuthorizationResult
-      const result = {
-        "status": ApplePaySession.STATUS_SUCCESS
-      };
-      session.completePayment(result);
-    };
-
-
-    session.oncancel = event => {
-      // Payment canceled by WebKit
-    };
-
-    session.begin();
+  if (!ApplePaySession) {
+    return;
   }
+
+  // Define ApplePayPaymentRequest
+  const request = {
+    "countryCode": "US",
+    "currencyCode": "USD",
+    "merchantCapabilities": [
+      "supports3DS"
+    ],
+    "supportedNetworks": [
+      "visa",
+      "masterCard",
+      "amex",
+      "discover"
+    ],
+    "total": {
+      "label": "Demo (Card is not charged)",
+      "type": "final",
+      "amount": "1.99"
+    }
+  };
+
+  // Create ApplePaySession
+  const session = new ApplePaySession(3, request);
+
+  session.onvalidatemerchant = async event => {
+    // Call your own server to request a new merchant session.
+    const merchantSession = await validateMerchant();
+    session.completeMerchantValidation(merchantSession);
+  };
+
+  session.onpaymentmethodselected = event => {
+    // Define ApplePayPaymentMethodUpdate based on the selected payment method.
+    // No updates or errors are needed, pass an empty object.
+    const update = {};
+    session.completePaymentMethodSelection(update);
+  };
+
+  session.onshippingmethodselected = event => {
+    // Define ApplePayShippingMethodUpdate based on the selected shipping method.
+    // No updates or errors are needed, pass an empty object.
+    const update = {};
+    session.completeShippingMethodSelection(update);
+  };
+
+  session.onshippingcontactselected = event => {
+    // Define ApplePayShippingContactUpdate based on the selected shipping contact.
+    const update = {
+      "newTotal": {
+        "label": "Demo (Card is not charged)",
+        "amount": "35.00"
+      },
+      "newLineItems": [
+        {
+          "label": "Subtotal",
+          "amount": "17.75"
+        },
+        {
+          "label": "Sales Tax",
+          "amount": "2.25"
+        },
+        {
+          "label": "Shipping",
+          "amount": "10.00"
+        }
+      ]
+    };
+    session.completeShippingContactSelection(update);
+  };
+
+  session.onpaymentauthorized = event => {
+    // Define ApplePayPaymentAuthorizationResult
+    const result = {
+      "status": ApplePaySession.STATUS_SUCCESS
+    };
+    session.completePayment(result);
+  };
+
+  session.oncouponcodechanged = event => {
+    // Define ApplePayCouponCodeUpdate
+    const newTotal = calculateNewTotal(event.couponCode);
+    const newLineItems = calculateNewLineItems(event.couponCode);
+    const newShippingMethods = calculateNewShippingMethods(event.couponCode);
+    const errors = calculateErrors(event.couponCode);
+
+    session.completeCouponCodeChange({
+      newTotal: newTotal,
+      newLineItems: newLineItems,
+      newShippingMethods: newShippingMethods,
+      errors: errors,
+    });
+  };
+
+  session.oncancel = event => {
+    // Payment canceled by WebKit
+  };
+
+  session.begin();
+}
 
 function applepay(){
 
