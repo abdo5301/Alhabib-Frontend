a8d703063028a095737a7aadfd7ece789022f0dc
alhabib shop <info@alhabibshop.com>
2024-01-29 10:15:38 +0300

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       // script: './.output/server/index.mjs'
       script: './start.js',
       env: {
-        HOST: '127.0.0.1',
+        HOST: 'localhost',
         PORT: 3000
       }
     }
