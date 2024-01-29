04c58a0536e6530d5643761c1fb31802f74c9c7e
alhabib shop <info@alhabibshop.com>
2024-01-29 10:34:09 +0300

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'front.test.alhabibshop.com',
-      // name: 'front.alhabibshop.com',
-      // port: '3000',
-      // exec_mode: 'cluster',
-      // instances: 'max',
-      // script: './.output/server/index.mjs'
-      script: './start.js',
-      env: {
-        HOST: 'localhost',
-        PORT: 3000
-      }
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+      // script: './start.js',
+      // env: {
+      //   HOST: 'localhost',
+      //   PORT: 3000
+      // }
     }
   ]
 }
