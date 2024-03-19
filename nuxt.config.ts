155b29bad2548b7440ed3079a8a385e8babdda39
Abdulrahman Saeed <abdelrahmansaeed3@gmail.com>
2024-03-19 17:10:47 +0200

    Add debounce to search function

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', 'nuxt-lodash'],
 
   i18n: {
     vueI18n: './i18n.config.ts',
