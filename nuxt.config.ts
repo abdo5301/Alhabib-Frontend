31330dfb950205ade145768cc9b367cb129fc72b
sulaiman1000 <sulaiman1000@users.noreply.github.com>
2024-06-03 23:11:16 +0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+  target: 'server',
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n',
